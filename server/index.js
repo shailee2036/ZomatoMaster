0e19d13e51a09e7b1d379f7e520be54cad3dd638
shailee2036 <shailee20edu@gmail.com>
2021-09-21 21:08:29 +0530

    first update

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+const zomato = express();
+
+zomato.use(express.json());
+zomato.use(express.urlencoded({extended: false}));
+zomato.use(helmet());
+zomato.use(cors());
+
+zomato.get("/", (req,res) => res.json({message:"setup success yay!!!"}));
+
+zomato.listen(4000, ()=> console.log("server is up and running"));
