24cb0f7479a6092bd05cde3088ee265ce0be54c7
Shailee Shah <83085462+shailee2036@users.noreply.github.com>
2021-10-19 10:17:55 +0530

    Update s3.js
    for security reasons

--- server/Utilis/AWS/s3.js
+++ server/Utilis/AWS/s3.js
@@ -2,8 +2,8 @@ import AWS from "aws-sdk";
 
 //AWS S3 bucket configuration
 const s3Bucket = new AWS.S3({
-  accessKeyId: "AKIAWIRUCJM7XOA4FJPK",
-  secretAccessKey: "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",
+  accessKeyId: "",
+  secretAccessKey: "",
   region: "ap-south-1"
 });
 
